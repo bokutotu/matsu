6675050937cb4f375864b407bbe623ab7db912ca
bokutotu <mushin.hudoushin@gmail.com>
2026-02-27 03:34:59 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "matsu";
+
+  nixConfig = {
+    "extra-substituters" = [
+      "https://cache.nixos.org"
+    ];
+    "extra-trusted-public-keys" = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.idris2
+          ];
+        };
+      });
+}
