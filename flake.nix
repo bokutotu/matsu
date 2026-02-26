dc97fce341d7e9b41c5c3240b3fe83c4552e91e5
bokutotu <mushin.hudoushin@gmail.com>
2026-02-27 03:52:36 +0900

    isl

--- flake.nix
+++ flake.nix
@@ -13,17 +13,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    isl-src = {
+      url = "git+https://repo.or.cz/isl.git?ref=refs/heads/master";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, isl-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        islLatest = pkgs.isl.overrideAttrs (old: {
+          version = "unstable";
+          src = isl-src;
+          nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.autoreconfHook ];
+        });
       in
       {
+        packages = {
+          isl-latest = islLatest;
+          default = islLatest;
+        };
+
         devShells.default = pkgs.mkShell {
+          buildInputs = [
+            islLatest
+          ];
+
           packages = [
             pkgs.idris2
+            pkgs.pkg-config
           ];
         };
       });
